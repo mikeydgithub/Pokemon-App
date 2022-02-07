3e2d9c8b9583d52df0a0ac50354a630a4b9c806c
dieres <dieres2010@gmail.com>
2022-02-07 10:25:34 -0500

    searched users stored to localStorage

--- assets/githubUser.js
+++ assets/githubUser.js
@@ -1,6 +1,8 @@
 var userFormEl = document.querySelector("#search-user-form");
 var nameInputEl = document.querySelector("#search-user");
 
+var githubUsr = [];
+
 var getUserRepos = function (user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -13,6 +15,24 @@ var getUserRepos = function (user) {
     });
 };
 
+var addUserName = function(user) {
+
+    var userExists = false;
+
+    // check if the user already exists
+    for (var i=0; i < githubUsr.length; i++) {
+        if (githubUsr[i] == user) {
+            userExists = true;
+        };
+    };
+    // if the user doesn't exists
+    if (!userExists) {
+        // add user to the end of githubUsr array and refresh localStorage ghUsers array
+        githubUsr[githubUsr.length] = user;
+        localStorage.setItem("ghUsers", JSON.stringify(githubUsr));
+    }
+};
+
 var formSubmitHandler = function (event) {
     event.preventDefault();
     // get value from input element
@@ -21,6 +41,8 @@ var formSubmitHandler = function (event) {
     if (username) {
         getUserRepos(username);
         nameInputEl.value = "";
+        // Add username (if new) to LocalStorage and githubUsr array
+        addUserName(username);
     } else {
         alert("Please enter a GitHub username");
     }

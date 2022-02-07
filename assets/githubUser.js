3b1a225ee29f569972391711ba2cf80204073e45
Adrian <adrianhenry2115@gmail.com>
2022-02-06 22:20:14 -0500

    johnnys style update

--- assets/githubUser.js
+++ assets/githubUser.js
@@ -1,5 +1,7 @@
 var userFormEl = document.querySelector("#search-user-form");
 var nameInputEl = document.querySelector("#search-user");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var getUserRepos = function (user) {
     // format the github api url
@@ -8,7 +10,7 @@ var getUserRepos = function (user) {
     // make a request to the url
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
@@ -26,6 +28,32 @@ var formSubmitHandler = function (event) {
     }
     console.log(event);
 };
+
+var displayRepos = function (repos, searchTerm) {
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
 
 
 

a8e2b899620772d69ee5795f812c06f4db5bf8c0
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-02-07 21:44:58 -0500

    for shared use

--- assets/githubuser.js
+++ assets/githubuser.js
@@ -1,6 +1,35 @@
 var userFormEl = document.querySelector("#search-user-form");
 var nameInputEl = document.querySelector("#search-user");
 
+var githubUsr = [];
+
+// append an user in the option list for the "Search for Partner" field
+var createUser = function(user) {
+
+    // append option to parent users
+    $("#users").append("<option value='" + user + "'>");
+    
+}
+
+// get the user names from localStorage to add as users in the list for the "Search for Partner" field
+var usersLs = JSON.parse(localStorage.getItem("ghUsers"));
+
+// if users in localStorage
+
+if (usersLs) {
+  // if cities in localStorage create city buttons
+  var i =0;
+  
+  // then loop over array to load tasks
+  $.each(usersLs, function() {
+          // asign values from local storage to task array
+          githubUsr[i] = usersLs[i];
+          createUser(githubUsr[i]);
+          i++;
+      });
+
+  };
+
 var getUserRepos = function (user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -13,6 +42,29 @@ var getUserRepos = function (user) {
     });
 };
 
+  
+// Add username to githubUsr Array to localStorage and create a new user in the list for the Search for Partner field
+
+var addUserName = function(user) {
+
+    var userExists = false;
+
+    // check if the user already exists
+    for (var i=0; i < githubUsr.length; i++) {
+        if (githubUsr[i] == user) {
+            userExists = true;
+        };
+    };
+    // if the user doesn't exists
+    if (!userExists) {
+        // add user to the end of githubUsr array and refresh localStorage ghUsers array
+        githubUsr[githubUsr.length] = user;
+        createUser(user);
+        localStorage.setItem("ghUsers", JSON.stringify(githubUsr));
+
+    }
+};
+
 var formSubmitHandler = function (event) {
     event.preventDefault();
     // get value from input element
@@ -20,7 +72,16 @@ var formSubmitHandler = function (event) {
 
     if (username) {
         getUserRepos(username);
+
+        // remove and create p element with username
+        $(".userGit").remove();
+        var userP= $("<P>").addClass("userGit").text(username);
+        $("#userName").append(userP);
+
         nameInputEl.value = "";
+        
+        // Add username (if new) to LocalStorage and githubUsr array
+        addUserName(username);
     } else {
         alert("Please enter a GitHub username");
     }

ba8674a32f67ea63d78cab8443434142c70c96f7
Adrian <adrianhenry2115@gmail.com>
2022-02-08 23:46:32 -0500

    status push check

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -8,9 +8,9 @@
 // 50-75= LvL 3 & No Evolution Pokemon
 // 75+= Legendary Pokemon
 
-const pokedex = document.getElementById("#pokedex");
-var gitUser = document.getElementById("#repo-search-term");
-var searchForm = document.getElementById("#search-form");
+const pokedex = document.getElementById("pokedex");
+var gitUser = document.getElementById("repo-search-term");
+var searchForm = document.getElementById("search-form");
 var userFormEl = document.querySelector("#search-user-form");
 var nameInputEl = document.querySelector("#search-user");
 var repoContainerEl = document.querySelector("#repos-container");
@@ -21,9 +21,9 @@ var lvl2Pokemon = ["2, 5, 8, 12, 15, 18, 24, 25, 28, 30, 33, 36, 40, 44, 47, 49,
 var lvl3Pokemon = ["3, 6, 9, 25, 31, 34, 38, 45, 62, 65, 68, 71, 73, 76, 78, 80, 82, 87, 89, 91, 84, 95, 97, 99, 101, 103, 105, 106, 107, 108, 110, 112, 113, 114, 115, 117, 119, 121, 122, 123, 124, 125, 126, 127, 128, 134, 135, 136, 137, 139, 141"]
 var legendaries = ["26, 130, 131, 132, 142, 143, 144, 145, 146, 147, 148, 149, 150"]
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
+});
 
 var getUserRepos = function (user) {
     // format the github api url
@@ -47,16 +47,17 @@ var getUser = function (user) {
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
             displayUser(data, user);
+            console.log(data)
         });
     });
 };
 
-var addUserName = function(user) {
+var addUserName = function (user) {
 
     var userExists = false;
 
     // check if the user already exists
-    for (var i=0; i < githubUsr.length; i++) {
+    for (var i = 0; i < githubUsr.length; i++) {
         if (githubUsr[i] == user) {
             userExists = true;
         };
@@ -90,6 +91,14 @@ var displayUser = function (repos, searchTerm) {
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
     // loop over repos
+    if (repos.public_repos = 1 && repos.public_repos <=10) {
+        fetchPokemon(lvl1Pokemon);
+    } else {
+        (repos.public_repos = 10 && repos.public_repos <=25)
+        fetchPokemon(lvl2Pokemon)
+        
+    }
+    console.log(repos.public_repos)
     for (var i = 0; i < repos.length; i++) {
         // format repo name
         var repoName = repos[i].owner.login + "/" + repos[i].name;
@@ -100,12 +109,13 @@ var displayUser = function (repos, searchTerm) {
 
         // append to container
         repoEl.appendChild(titleEl);
+
     }
 };
 
-const fetchPokemon = () => {
+var fetchPokemon = () => {
 
-    const promises = [];
+    var promises = [];
     for (let i = 1; i <= 150; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         // push promise that is returned from doing the fetch and .then
@@ -117,26 +127,35 @@ const fetchPokemon = () => {
     // iterating through each result 
     // with each one of those its gonna transform it into an object in the form of our choosing
     Promise.all(promises).then((results) => {
-        const pokemon = results.map((data) => ({
+        var pokemon = results.map((data) => ({
             name: data.name,
             id: data.id,
             image: data.sprites['front_default'],
             type: data.types.map((type) => type.type.name).join(', ')
 
         }));
-        var randPokeman = pokemon[Math.floor(Math.random() * pokemon.length)]
-
-        displayPokemon(randPokeman);
+        function getPokemanId(id) {
+            var result = '';
+            for(var i = 1; i <=lvl1Pokemon; i++) {
+                if(id === lvl1Pokemon){
+                    result += `${pokemon}.${id} = ${id[i]}`
+                }
+            }
+            var randPokeman = pokemon[Math.floor(Math.random() * pokemon.length)]
+            
+            displayPokemon(randPokeman);
+        }
+        getPokemanId()
     });
-
+    
 };
 
 
 
-const displayPokemon = (pokeman) => {
+var displayPokemon = (pokeman) => {
     console.log(pokeman);
-    const pokemonHTMLString = 
-    `
+    var pokemonHTMLString =
+        `
     <li>
         <img src="${pokeman.image}"/>
         <h2>${pokeman.id}. ${pokeman.name}</h2>
@@ -148,5 +167,5 @@ const displayPokemon = (pokeman) => {
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
-fetchPokemon();
+//fetchPokemon();
 
